083bee3f2e937f046e89fe878380459929a3c06b
vsarrocchi <vsarrocchi@users.noreply.github.com>
2022-12-22 10:50:41 +0000

    Update platform deliverables

--- styles/design-tokens.js
+++ styles/design-tokens.js
@@ -1,7 +1,7 @@
 /**
  * Do not edit directly
- * Generated on Thu, 22 Dec 2022 10:24:51 GMT
+ * Generated on Thu, 22 Dec 2022 10:50:40 GMT
  */
 
-export const tokenButtonColorBackground = "#00fdff";
+export const tokenButtonColorBackground = "#02ff00";
 export const tokenTokenSetOrder0 = "global";
